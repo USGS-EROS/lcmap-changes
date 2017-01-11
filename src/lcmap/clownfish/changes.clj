49fccd90a4f79ad3e5ddc634682b83be953f2053
davidvhill <davehill75@gmail.com>
2017-01-11 14:32:53 -0600

    server is starting in dev, html is rendering

--- src/lcmap/clownfish/changes.clj
+++ src/lcmap/clownfish/changes.clj
@@ -1,1 +1,77 @@
-(ns lcmap.clownfish.changes)
+(ns lcmap.clownfish.changes
+  (:require [camel-snake-kebab.core :refer [->snake_case_keyword]]
+            [camel-snake-kebab.extras :refer [transform-keys]]
+            [cheshire.core :as json]
+            [clojure.tools.logging :as log]
+            [clojure.string :as str]
+            [compojure.core :refer :all]
+            [ring.util.accept :refer [accept]]
+            [lcmap.clownfish.html :as html]
+            [lcmap.clownfish.middleware :refer [wrap-handler]]))
+
+(defn allow [& verbs]
+  (log/debug "explaining allow verbs")
+  {:status 405
+   :headers {"Allow" (str/join "," verbs)}})
+
+;;; Request entity transformers.
+
+(defn decode-json
+  ""
+  [body]
+  (log/debug "req - decoding as JSON")
+  (->> body
+       (slurp)
+       (json/decode)
+       (transform-keys ->snake_case_keyword)))
+
+(defn prepare-with
+  "Request transform placeholder."
+  [request]
+  (log/debugf "req - prepare body: %s" (get-in request [:headers]))
+  (if (= "application/json" (get-in request [:headers "content-type"]))
+    (update request :body decode-json)
+    request))
+
+;;; Response entity transformers.
+
+(defn to-html
+  "Encode response body as HTML."
+  [response]
+  (log/debug "responding with HTML")
+  (let [template-fn (:template (meta response) html/default)]
+    (update response :body template-fn)))
+
+(defn to-json
+  "Encode response body as JSON."
+  [response]
+  (log/debug "responding with json")
+  (-> response
+      (update :body json/encode)
+      (assoc-in [:headers "Content-Type"] "application/json")))
+
+(def supported-types (accept "text/html" to-html
+                             "application/json" to-json
+                             "*/*" to-json))
+
+(defn respond-with
+  ""
+  [request response]
+  (supported-types request response))
+
+;;; Routes
+
+(defn resource
+  "Handlers for landsat resource."
+  []
+  (wrap-handler
+   (context "/changes" request
+     (GET    "/" []
+             (with-meta {:status 200}
+               {:template html/default}))
+     (ANY    "/" []
+             (with-meta (allow ["GET"])
+               {:template html/default}))
+     (GET    "/problem/" []
+             {:status 200 :body "problem resource"}))
+   prepare-with respond-with))
---
+++ src/lcmap/clownfish/server.clj
@@ -0,0 +1,99 @@
+(ns lcmap.clownfish.server
+  "Clownfish HTTP server related functions.
+
+  This namespace provides functions and states for running the
+  LCMAP-Changes REST API."
+  (:require [cheshire.core :refer :all]
+            [cheshire.generate :as json-gen :refer [add-encoder]]
+            [compojure.core :refer :all]
+            [clojure.tools.logging :as log]
+            [langohr.exchange :as le]
+            [langohr.queue :as lq]
+            [lcmap.clownfish.config :refer [config]]
+            [lcmap.clownfish.db :as db :refer [db-session]]
+            [lcmap.clownfish.event :as event :refer [amqp-channel]]
+            [lcmap.clownfish.middleware :refer [wrap-authenticate wrap-authorize]]
+            [lcmap.clownfish.problem :as problem]
+            [lcmap.clownfish.changes :as changes]
+            [mount.core :refer [defstate] :as mount]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.accept :refer [wrap-accept]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.problem :refer [wrap-problem]]
+            [ring.middleware.resource :refer [wrap-resource]])
+
+  (:import [org.joda.time.DateTime]
+           [org.apache.commons.codec.binary Base64]))
+
+;;; This is the REST API entrypoint. All general middleware
+;;; should be added here. Subordinate resources should be
+;;; defined in other namespaces.
+
+(defn make-handler
+  "Build a middleware wrapped handler for app. This approach makes
+  dependencies (components) available to handling functions."
+  []
+  (-> (changes/resource)
+      (wrap-resource "public")
+      (wrap-accept)
+      (wrap-authorize)
+      (wrap-authenticate)
+      (wrap-keyword-params)
+      (wrap-params)
+      (wrap-problem problem/transformer)))
+
+;;; Server-related state
+
+(defstate ring-handler
+  :start (do
+           (log/debugf "starting Ring handler")
+           (make-handler)))
+
+(defstate server
+  :start (let [args (get-in config [:http])]
+           (log/debugf "starting Jetty: %s" args)
+           (run-jetty ring-handler args))
+  :stop  (do
+           (log/debugf "stopping Jetty")
+           (.stop server)))
+
+;; The exchange to which the server publishes messages. This
+;; is used to indirectly notify the worker of events that will
+;; result in processing data.
+
+(defstate server-exchange
+  :start (let [exchange-name (get-in config [:server :exchange])]
+           (log/debugf "creating server exchange: %s" exchange-name)
+           (le/declare amqp-channel exchange-name "topic" {:durable true})))
+
+;; The queue from which the server might eventually consume
+;; messages. There are currently no bindings or consumers,
+;; this is really just a placeholder.
+
+(defstate server-queue
+  :start (let [queue-name (get-in config [:server :queue])]
+           (log/debugf "creating server queue: %s" queue-name)
+           (lq/declare event/amqp-channel queue-name {:durable true
+                                                      :exclusive false
+                                                      :auto-delete false})))
+
+;; Encoders; turn objects into strings suitable for JSON responses.
+
+(defn iso8601-encoder
+  "Transform a Joda DateTime object into an ISO8601 string."
+  [date-time generator]
+  (.writeString generator (str date-time)))
+
+(defn base64-encoder
+  "Base64 encode a byte-buffer, usually raster data from Cassandra."
+  [buffer generator]
+  (log/debug "encoding HeapByteBuffer")
+  (let [size (- (.limit buffer) (.position buffer))
+        copy (byte-array size)]
+    (.get buffer copy)
+    (.writeString generator (Base64/encodeBase64String copy))))
+
+(json-gen/add-encoder org.joda.time.DateTime iso8601-encoder)
+
+(json-gen/add-encoder java.nio.HeapByteBuffer base64-encoder)
