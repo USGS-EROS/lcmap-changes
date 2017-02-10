859b1b40849acae93ded8afae7c2421259f91dd1
davidvhill <davehill75@gmail.com>
2017-02-09 23:47:29 -0600

    added comments

--- src/lcmap/clownfish/setup/initialize.clj
+++ src/lcmap/clownfish/setup/initialize.clj
@@ -11,6 +11,39 @@
             [lcmap.clownfish.setup.event]
             [mount.core :as mount]))
 
+(comment
+ "In it's initial state, the db does not have a schema and rabbitmq does
+ not have queues, bindings or exchanges. These can be created manually:
+
+ user=> (require '[lcmap.clownfish.setup.initialize :as init])
+ user=> (init/db environment)
+ user=> (init/eventing environment)
+
+ If run from the repl, this will create whatever cassandra schema is provided
+ in dev/resources/schema.setup.cql, and whatever rabbitmq exchanges, queues and
+ bindings are contained in dev/resources/rabbit-setup.edn.
+
+ If run from the test profile the same holds true except the file path is
+ test/resources/schema.setup.cql and test/resources/rabbit-setup.edn.
+
+ (start), (stop) and (bounce) may be called from the repl following
+ initialization. lcmap.clownfish.system is hard wired to never start
+ lcmap.clownfish.setup.db or lcmap.clownfish.setup.event states.
+ These are only ever started through initialize.clj.
+
+ initialize.clj *can* be used to configure remote systems by manipulating the
+ environment parameter.  Additional steps are necessary to create
+ anything that doesn't reference the 'local' or 'unit' environments, as both
+ cassandra schemas (in dev and test) and the rabbitmq constructs are
+ using qualified names that indicate either an environment of 'local' or 'unit'.
+
+ If this is something you really want to do, examine
+ dev/resources/environment.clj and construct a map with values altered as
+ needed.  lcmap.clownfish.setup.* is hard-wired to look for schema.setup.cql,
+ schema.teardown.cql and rabbit-setup.edn on the resource paths.  This was done
+ intentionally to prevent accidental modifications to remote environments.
+")
+
 (def system-initialization nil)
 (def retry-strategy (again/max-retries 0 (again/constant-strategy 0)))
 
