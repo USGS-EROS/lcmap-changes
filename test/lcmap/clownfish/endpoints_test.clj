14ac471f14f6296a63f809ce6c789b6add2dc940
davidvhill <davehill75@gmail.com>
2017-02-16 13:12:11 -0600

    initial set of algorithm endpoint tests are in place and working.

--- test/lcmap/clownfish/endpoints_test.clj
+++ test/lcmap/clownfish/endpoints_test.clj
@@ -9,6 +9,9 @@
             [lcmap.clownfish.server :as server]
             [lcmap.clownfish.shared :refer [with-system req]]))
 
+(def json-header {"Accept" "application/json"
+                  "Content-Type" "application/json"})
+
 (deftest changes-health-resource
   (with-system
     (testing "health check"
@@ -19,43 +22,61 @@
 (deftest algorithms
   (with-system
     (testing "no algorithms defined"
-      (let [resp (log/spy (req :get (str http-host "/algorithms")
-                               :headers {"Accept" "application/json"}))
+      (let [resp (log/spy :debug (req :get (str http-host "/algorithms")
+                                      :headers {"Accept" "application/json"}))
             body (json/decode (:body resp))]
         (is (= 200 (:status resp)))
         (is (= 0 (count body)))
         (is (coll? body))))
 
     (testing "add algorithm - bad bodies"
-      (let [headers {"Accept" "application/json"
-                     "Content-Type" "application/json"}]
-          (doseq [body [{:enabled true :bad-inputs_url_template "http://host"}
-                        {:not-enabled true :inputs_url_template "http://host"}
-                        {:enabled "string" :inputs_url_template "http://host"}]]
-              (let [response (req :put (str http-host "/algorithm/bad")
-                                  :body (json/encode body)
-                                  :headers headers)
-                    status (:status response)]
+        (doseq [body [{:enabled true :bad-inputs_url_template "http://host"}
+                      {:not-enabled true :inputs_url_template "http://host"}
+                      {:enabled "string" :inputs_url_template "http://host"}]]
+            (let [response (req :put (str http-host "/algorithm/bad")
+                                :body (json/encode body)
+                                :headers json-header)
+                  status (:status response)]
                 (when (not (contains? #{403 500} status))
-                  (log/errorf "Error in add algorithm-bad bodies")
-                  (log/errorf "Response:%s" response))
-                (is (contains? #{403 500} status))))))
+                    (log/errorf "Error in add algorithm-bad bodies")
+                    (log/errorf "Response:%s" response))
+                (is (contains? #{403 500} status)))))
 
-    (testing "add good algorithms"
-      (let [resp (req :get (str http-host "/algorithms"))]
-        ()))
+    (testing "add good algorithm"
+      (let [body   {:enabled false :inputs_url_template "http://host"}
+            resp   (req :put (str http-host "/algorithm/good")
+                        :body (json/encode body)
+                        :headers json-header)
+            status (:status resp)]
+          (is (= 202 status))))
 
-    (testing "update algorithm"
-      (let [resp (req :get (str http-host "/algorithms"))]
-        ()))
+    (testing "update algorithm")
+    (let [body     {:enabled true :inputs_url_template "http://anotherhost"}
+          response (req :put (str http-host "/algorithm/good")
+                        :body (json/encode body)
+                        :headers json-header)
+          status   (:status response)]
+        (is (= 202 status)))
 
     (testing "get algorithm"
-      (let [resp (req :get (str http-host "/algorithm/test-alg-1"))]
-        ()))
+      (let [resp     (req :get (str http-host "/algorithm/good"))
+            status   (:status resp)
+            body     (:body resp)
+            expected {:algorithm "good"
+                      :enabled true
+                      :inputs_url_template "http://anotherhost"}]
+        (is (= 200 status))
+        (is (= (json/decode body true) expected))))
 
     (testing "get algorithms"
-      (let [resp (req :get (str http-host "/algorithms"))]
-        ()))))
+      (let [resp   (req :get (str http-host "/algorithms"))
+            status (:status resp)
+            body   (:body resp)
+            expected [{:algorithm "good"
+                       :enabled true
+                       :inputs_url_template "http://anotherhost"}]]
+       (is (= 200 status))
+       (is (= (json/decode body true) expected))))))
 
 (deftest results
   (with-system
